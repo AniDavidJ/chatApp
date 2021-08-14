89d08e2caf3d7fdf10dcdd0fe05b63039c3f5e2c
AniDavidJ <anidavid07@gmail.com>
2021-08-14 14:22:26 +0400

    chat

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -1,5 +1,12 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
 const ChatFeed =(props) => {
-    console.log(props);
+    const { chats, activeChat , userName, message} = props
+    const chat = chats && chats[activeChat];
+
+    console.log(chat, userName,message);
     return(
         <div>ChatFeed</div>
     )
